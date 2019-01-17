3bd3a80065aa35ff0706325632bc1480cc3f1a86
koinexBot <46602174+koinexBot@users.noreply.github.com>
2019-01-17 23:28:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -512,7 +512,7 @@ client.on('message' , message => {
     var prefix = "$";
     let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
     if(message.content.startsWith(prefix + 'unban')) {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('** You Dont have `ADMINISTRATOR ` Permission ! ** ❌);
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('** You Dont have `ADMINISTRATOR ` Permission ! ** ❌');
         if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
         message.guild.unban(user);
         message.guild.owner.send(`**unbanned successful \n ${user} \n By : <@${message.author.id}>**`)
